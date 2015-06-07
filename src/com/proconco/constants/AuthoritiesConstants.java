b0bb1a0c0693535f09c3befe57e1e48a200b0b33
Administrator <Administrator@192.168.1.66>
2015-06-06 17:56:48 -0700

    Group

---
+++ src/com/proconco/constants/AuthoritiesConstants.java
@@ -0,0 +1,22 @@
+package com.proconco.constants;
+
+/**
+ * The Class AuthoritiesConstants.
+ */
+public class AuthoritiesConstants {
+
+	/**
+	 * Instantiates a new authorities constants.
+	 */
+	private AuthoritiesConstants() {
+    }
+
+    /** The Constant ADMIN. */
+    public static final String ADMIN = "ROLE_ADMIN";
+
+    /** The Constant USER. */
+    public static final String USER = "ROLE_USER";
+
+    /** The Constant ANONYMOUS. */
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+}
